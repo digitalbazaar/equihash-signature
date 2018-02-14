62431b45b2e4946fd0aba02f3eb02a6a120fe9c1
Dave Longley <dlongley@digitalbazaar.com>
2018-02-14 18:28:27 -0500

    Add `verify` test and fix bugs to pass it.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -7,23 +7,41 @@ const equihashSigs = require('equihash-signature');
 const jsigs = require('jsonld-signatures');
 equihashSigs.install(jsigs);
 
+const testDoc = {
+  "@context": "https://w3id.org/security/v2",
+  "id": "foo:something",
+  "proof": {
+    "type": "EquihashProof2018",
+    "created": "2018-02-14T23:27:45Z",
+    "equihashParameterK": 3,
+    "equihashParameterN": 64,
+    "nonce": "AQAAAA==",
+    "proofValue": "AABBiAAAnTAAALM8AAGc0AAArs4AAaiYAAE2HQABvyw="
+  }
+};
+
 describe('Equihash Signature API', () => {
-  describe('sign', () => {
-    it('signs something', done => {
-      jsigs.sign({
-        '@context': 'https://w3id.org/security/v2',
-        id: 'foo:something'
-      }, {
-        algorithm: 'EquihashProof2018',
-        parameters: {
-          n: 64,
-          k: 3
-        }
-      }, (err, result) => {
-        // FIXME: is this assertion accurate?
-        result.proof.proofValue.should.not.equal('');
-        done();
-      });
+  it('sign', done => {
+    jsigs.sign({
+      '@context': 'https://w3id.org/security/v2',
+      id: 'foo:something'
+    }, {
+      algorithm: 'EquihashProof2018',
+      parameters: {
+        n: 64,
+        k: 3
+      }
+    }, (err, result) => {
+      // FIXME: is this assertion accurate?
+      result.proof.proofValue.should.not.equal('');
+      done();
+    });
+  });
+
+  it('verify', done => {
+    jsigs.verify(testDoc, (err, result) => {
+      result.verified.should.equal(true);
+      done();
     });
   });
 });
